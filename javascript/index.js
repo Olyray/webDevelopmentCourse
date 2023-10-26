948358041a2c9034bdf70630f42caab0f5ffdd69
Olamide <olire96@gmail.com>
2023-10-26 13:13:32 +0100

    tinDog and BootStrap

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+function fibonacciGenerator(n) {
+  //Do NOT change any of the code above 👆
+  
+  //Write your code here:
+  var fibonacci = [];
+  
+  if (n === 1) {
+    fibonacci = [0];
+  } else if (n === 2) {
+    fibonacci = [0, 1];
+  } else {
+    fibonacci = [0, 1];
+    for (var i = 2; i < n; i++) {
+      fibonacci.push(fibonacci[i - 2] + fibonacci[i - 1]);
+    }
+  }
+  
+  //Return an array of fibonacci numbers starting from 0.
+  return fibonacci;
+  
+  //Do NOT change any of the code below 👇
+}
+
+console.log(fibonacciGenerator(5));
