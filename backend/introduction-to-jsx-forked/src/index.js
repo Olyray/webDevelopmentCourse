0609a2daec0a5ee94d5782690d3f1fed045a87fa
Olamide <olire96@gmail.com>
2023-11-21 20:20:08 +0100

    Added introduction to JSX and Secrets

---
+++ backend/introduction-to-jsx-forked/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph.</p>
+    <p> This is another paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
